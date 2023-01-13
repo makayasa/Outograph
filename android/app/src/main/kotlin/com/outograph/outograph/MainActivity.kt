8ff80079c2b34ce71dce454a4fe7133fcbcff9d4
Makayasa <rifqimakayasa@gmail.com>
2023-01-13 09:09:29 +0700

    change package name

--- android/app/src/main/kotlin/com/outograph/outograph/MainActivity.kt
+++ android/app/src/main/kotlin/com/outograph/outograph/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.outograph.outograph
-
+//package com.outograph.outograph
+package com.outograph
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
